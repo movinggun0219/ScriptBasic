2dcd3f5cb7e15725668e3ba7401c90f39ceabe0e
movinggun0219 <daugun@naver.com>
2025-07-08 15:36:33 +0900

    7/8

---
+++ Assets/Scripts/VariablesAndFunctions.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    // 클래스 내에서 변수 위치는 상관없다.
+    int myInt = 5;
+
+    void Start()
+    {
+        myInt = MultiplyByThree(myInt);
+        Debug.Log(myInt);
+    }
+
+
+    int MultiplyByThree(int number)
+    {
+        int result;
+        result = number * 3;
+        return result;
+    }
+    
+}
