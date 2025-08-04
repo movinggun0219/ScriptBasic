95607542f1877402fba57d67fda232cd0946303d
movinggun0219 <daugun@naver.com>
2025-08-04 15:48:10 +0900

    08/04

---
+++ Assets/01.Scripts/FormatStringTest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FormatStringTest : MonoBehaviour
+{
+    void Start() 
+    { 
+        string[] names = { "Iron Man", "Super Man" }; 
+        int[] ages = { 55, 100 }; 
+
+        Debug.LogFormat("{0,-20} {1,5}", "Name", "Age"); 
+        Debug.LogFormat("{0,-20} {1,5:N1}", names[0], ages[0]); 
+        Debug.LogFormat("{0,-20} {1,5:N1}", names[1], ages[1]); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
