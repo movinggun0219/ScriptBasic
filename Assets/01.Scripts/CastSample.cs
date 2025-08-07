9b6e3f95d601662ae9dcd5b9f6c7f288d19ec1d8
movinggun0219 <daugun@naver.com>
2025-08-07 15:44:45 +0900

    08/07

---
+++ Assets/01.Scripts/CastSample.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastSample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // int i1 = 1;
+        // float f1 = i1;
+        // float x = 1.234f;
+        // int a = (int)x;
+        // byte[] bytes = { 0, 1, 2, 34 };
+        // int i = BitConverter.ToInt32(bytes, 0);
+
+        string s1 = "12345"; //"abc"  << false
+        int i1 = default(int);
+        bool result = int.TryParse(s1,out i1);
+        Debug.Log($"try pase : {result} , i1 =  {i1}");
+
+        int i2 = Convert.ToInt32(s1);
+        Debug.Log($"Convert : {i1} ");
+
+        string s2 = "123.45"; 
+        float f2 = float.Parse(s2); 
+        Debug.Log(f2.ToString()); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
