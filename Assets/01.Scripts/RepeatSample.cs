9da5b425b1214a53ae79d014e96bc44babdffde2
movinggun0219 <daugun@naver.com>
2025-09-10 15:50:41 +0900

    09/10

---
+++ Assets/01.Scripts/RepeatSample.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatSample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // for (int i = 0; i < 5; i++)
+        // {
+        //     for (int j = 0; j < 4; j++)
+        //     {
+        //         Debug.Log($"i= {i} , j ={j}");
+        //     }
+        // }
+        // for (int i = 0; i < 5; i++)
+        // {
+        //     Debug.Log(i.ToString());
+        // }
+
+        // for (int i = 0, j = 0; i < 5 && j < 4; i++, j++)
+        // {
+        //     Debug.Log($"i= {i} , j ={j}");
+        // }
+    }
+
+}
